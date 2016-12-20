46c3ab46092a35a95bf64c882c05a204c36736b7
Feb Dao <hien.dao@ffwagency.com>
2016-12-20 10:50:03 +0700

    Add jcarousel library

--- js/jquery.js
+++ js/jquery.js
@@ -1,54 +1,17 @@
 $( document ).ready(function() {
-  $('.fgalery__for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    fade: true,
-    asNavFor: '.fgalery__nav',
-    infinite: false,
+  $('.fgalery__for').jcarousel({
+    items: '.fgalery__item'
   });
 
-  $('.fgalery__nav').slick({
-    asNavFor: '.fgalery__for',
-    arrows: true,
-    focusOnSelect: true,
-    infinite: false,
-    verticalSwiping: true,
-    slidesToShow: 18,
-    slidesToScroll: 18,
-    vertical: true,
-    responsive: [
-      {
-        breakpoint: 1400,
-        settings: {
-          slidesToShow: 13,
-          slidesToScroll: 13
-        }
-      },
-      {
-        breakpoint: 1023,
-        settings: {
-          slidesToShow: 10,
-          slidesToScroll: 10,
-          vertical: false,
-          verticalSwiping: false,
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 5,
-          slidesToScroll: 5,
-          vertical: false,
-          verticalSwiping: false,
-        }
-      }
-    ]
+  $('.fgalery__nav').jcarousel({
+    items: '.fgalery__item',
+    vertical: true
   }).on("mousewheel", function (event) {
     event.preventDefault();
     if (event.deltaX > 0 || event.deltaY < 0) {
-      $('.fgalery__for').slick("slickNext");
+      //$('.fgalery__for').slick("slickNext");
     } else if (event.deltaX < 0 || event.deltaY > 0) {
-      $('.fgalery__for').slick("slickPrev");
+      //$('.fgalery__for').slick("slickPrev");
     }
   });
 });
